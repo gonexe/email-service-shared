8aecc8ec4c4d659852e84fe45d1552c3e375c1da
gonexe <gon.exe@gmail.com>
2024-10-15 12:05:54 -0300

    Update shared library

---
+++ dist/types/ProviderType.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ProviderType = void 0;
+var ProviderType;
+(function (ProviderType) {
+    ProviderType["SendGrid"] = "SendGrid";
+    ProviderType["Mailgun"] = "Mailgun";
+})(ProviderType || (exports.ProviderType = ProviderType = {}));
